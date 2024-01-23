12d0b9124a957726f543ed2849ff76615aa194dc
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-01-23 15:44:23 +0300

    queue updated. isEmpty, getLength methods added.

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -9,13 +9,16 @@ class Node<T>{
 export default class Queue<T> {
     private head?: Node<T>;
     private tail?: Node<T>;
+    private length: number;
     constructor() {
         this.head = undefined;
         this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
         const node = new Node(item);
+        this.length++;
         if (!this.tail) {
             // no tail, queue is empty
             this.head = node;
@@ -29,12 +32,19 @@ export default class Queue<T> {
         if (!this.head) {
             return undefined;
         }
+        this.length--;
         // save current head
         const head = this.head;
         // move head to the next
         this.head = this.head.next;
         // return saved head value
         return head.value;
+    }
+    getLength(): number {
+        return this.length;
+    }
+    isEmpty(): boolean {
+        return this.length === 0;
     }
 
     peek(): T | undefined {
